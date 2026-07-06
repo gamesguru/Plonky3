e70e817a64211576ca60661dabaf8484e2646187
Shane Jaroch <sjjaroch@oakland.edu>
2026-07-06 14:42:03 -0400

    fix clippy lint:
    	cargo clippy --fix --lib -p p3-brakedown --tests -- -W clippy::perf -W clippy::pedantic --no-deps

--- brakedown/src/brakedown_code.rs
+++ brakedown/src/brakedown_code.rs
@@ -126,7 +126,6 @@ mod tests {
     use p3_field::PrimeCharacteristicRing;
     use p3_mersenne_31::Mersenne31;
     use rand::SeedableRng;
-    use rand_chacha::ChaCha20Rng;
 
     use super::*;
     use crate::macros::{brakedown, brakedown_to_dense};
